adbc5afc967a74b1fc6c09a0f7f15f383aa7063b
ydmins <dr.ydmins@gmail.com>
2023-06-03 19:47:59 +0900

    Canvas 사이즈 설정하기

--- Particle/index.js
+++ Particle/index.js
@@ -2,5 +2,16 @@ const canvas = document.querySelector('canvas');
 
 const ctx = canvas.getContext('2d');
 
+const canvasWidth = 300;
+const canvasHeight = 300;
+
+// Script에서 CSS 값 설정하기
+canvas.style.width = canvasWidth + 'px';
+canvas.style.height = canvasHeight + 'px';
+
+// Canvas의 고유 값 설정하기
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
 // 사각형 그리기
 ctx.fillRect(10, 10, 50, 50);
