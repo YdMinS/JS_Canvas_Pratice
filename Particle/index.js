9577c6e16fc31b1c34e6ca6987fefb817823a395
ydmins <dr.ydmins@gmail.com>
2023-06-03 19:51:57 +0900

    dpr(dvice pixel ratio) 적용하기

--- Particle/index.js
+++ Particle/index.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('canvas');
 
 const ctx = canvas.getContext('2d');
+const dpr = window.devicePixelRatio;
 
 const canvasWidth = 300;
 const canvasHeight = 300;
@@ -10,8 +11,9 @@ canvas.style.width = canvasWidth + 'px';
 canvas.style.height = canvasHeight + 'px';
 
 // Canvas의 고유 값 설정하기
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
+canvas.width = canvasWidth * dpr;
+canvas.height = canvasHeight * dpr;
+ctx.scale(dpr, dpr);
 
 // 사각형 그리기
 ctx.fillRect(10, 10, 50, 50);
